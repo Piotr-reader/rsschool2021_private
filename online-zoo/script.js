7d092a803434cec777b34fdb5a4a207703bc7582
Piotr-reader <piotr_partsianko@inbox.ru>
2021-03-25 15:40:19 +0300

    feat: add normalize css

---
+++ online-zoo/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const SWITCH_BOX = document.querySelector('.switch');
+let slider = document.querySelector('.slider');
+console.log(slider);
+SWITCH_BOX.addEventListener('mousedown', () => {
+
+});
