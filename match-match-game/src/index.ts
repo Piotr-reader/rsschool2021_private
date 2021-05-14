0b88cf4caf272776196967a0f343da86931dfc76
Piotr-reader <piotr_partsianko@inbox.ru>
2021-05-14 18:25:00 +0300

    fix: fix typescript plugin webpack

---
+++ match-match-game/src/index.ts
@@ -0,0 +1,7 @@
+import './styles/style.sass';
+// const img = require('./assets/card.jpg')
+// document.body.innerHTML = `
+// <img src="${img}" alt="card">
+// `;
+
+console.log('test');
--- match-match-game/webpack.config.js
+++ match-match-game/webpack.config.js
@@ -1,15 +1,17 @@
 const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlagin = require('copy-webpack-plugin');
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
-  entry: './index.js',
+  entry: './index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    assetModuleFilename: 'assets/[hash][ext]',
   },
   resolve: {
     extensions: [ '.js', '.ts', '.png', 'jpg', '.svg']
@@ -28,27 +30,38 @@ module.exports = {
       }),
       new CleanWebpackPlugin(),
       new MiniCssExtractPlugin({
-          filename: 'bundle.css',
-      })
+          filename: '[name].[contenthash].css',
+      }),
+      // new CopyPlagin({
+      //   patterns: [
+      //     { from: './public'}
+      //   ]
+      // })
   ],
   module: {
       rules: [
           {
-              test: /\.sass$/,
-              use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+              test: /\.css$/i,
+              use: [MiniCssExtractPlugin.loader, 'css-loader']
           },
           {
+            test: /\.sass$/i,
+            use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        },
+          {
             test: /\.(jpg|svg|png)$/,
-            use: ['file-loader']
+            type: 'asset/resource',
+            use: ['file-loader'],
           },
           {
             test: /\.(ttf|woff|woff2|eot)$/,
-            use: ['file-loader']
+            type: 'asset/resource',
+            use: ['file-loader'],
           },
           {
             test: /\.ts$/,
             use: ['ts-loader'],
-            exclude: /node_modules/
+            exclude: /node_modules/,
           }
       ]
   }
